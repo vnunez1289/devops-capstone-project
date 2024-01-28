63eda8c378fc5fe4c0ad0ae182bc87f6fbe0a57d
vnunez1289 <pvn1289@gmail.com>
2024-01-28 16:20:04 -0500

    Added read accounts function

--- service/routes.py
+++ service/routes.py
@@ -75,9 +75,17 @@ def list_accounts():
 ######################################################################
 # READ AN ACCOUNT
 ######################################################################
-
-# ... place you code here to READ an account ...
-
+@app.route("/accounts/<int:account_id>", methods=["GET"])
+def get_accounts(account_id):
+    """
+    Reads an Account
+    This endpoint will fetch an account based on the requested account_id.
+    """
+    app.logger.info("Request to read an Account")
+    account = Account.find(account_id)
+    if not account:
+        abort(status.HTTP_404_NOT_FOUND, f"Account with id [{account_id}] could not be found.")
+    return account.serialize(), status.HTTP_200_OK    
 
 ######################################################################
 # UPDATE AN EXISTING ACCOUNT
--- tests/test_routes.py
+++ tests/test_routes.py
@@ -137,5 +137,16 @@ class TestAccountService(TestCase):
         self.assertEqual(len(data), 5)
 
     def test_get_account_not_found(self):
+        """It should not return a nonexistant Account"""
         resp = self.client.get(f"{BASE_URL}/0")
         self.assertEqual(resp.status_code, status.HTTP_404_NOT_FOUND)
+
+    def test_get_account(self):
+        """It should Read a single Account"""
+        account = self._create_accounts(1)[0]
+        resp = self.client.get(
+            f"{BASE_URL}/{account.id}", content_type="application/json"
+        )
+        self.assertEqual(resp.status_code, status.HTTP_200_OK)
+        data = resp.get_json()
+        self.assertEqual(data["name"], account.name)
